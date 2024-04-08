cf61973391025690c0927fa490532f5df8611339
Susanna Sargsyan <153443229+susannasargsyan1@users.noreply.github.com>
2024-04-08 17:35:51 +0400

    Add files via upload

---
+++ DataBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace Library
+{
+    internal class DataBase
+    {
+        
+        private SqlConnection sqlConnection = new SqlConnection(@"Data Source=DESKTOP-AHJPH50\SQLEXPRESS01; Initial Catalog=Library; Integrated Security=True");
+
+        
+        public void openConnection()
+        {
+            try
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                    Console.WriteLine("Connection opened successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error opening connection: " + ex.Message);
+            }
+        }
+
+       
+        public void closeConnection()
+        {
+            try
+            {
+                if (sqlConnection.State == System.Data.ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                    Console.WriteLine("Connection closed successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error closing connection: " + ex.Message);
+            }
+        }
+
+        public SqlConnection getSqlConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
